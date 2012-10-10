e206f6d65f7ae16d564abc2dc5f3b8adbd790cd8
Joseph W. Breu <breu@breu.org>
2012-10-10 14:18:56 -0500

    clean up one line

--- rpcs/functions.sh
+++ rpcs/functions.sh
@@ -615,7 +615,7 @@ function run_chef() {
 function setup_chef_initscripts() {
     echo "setting up chef-client init scripts..."
     cp `find /opt/chef/embedded -print | grep "debian.*init/chef-client.conf" | head -n1` /etc/init/chef.conf
-    cp `find /opt/chef/embedded -print | grep "debian.*default/chef-client" | head -n 1` /etc/default/chef-client
+    cp `find /opt/chef/embedded -print | grep "debian.*default/chef-client" | head -n1` /etc/default/chef-client
     mkdir -p /var/log/chef
     /etc/init.d/chef-client start
     /usr/sbin/update-rc.d chef-client defaults
