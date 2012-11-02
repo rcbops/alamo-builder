rcbops/alamo-builder

commit 4de19f66533a7a4f1d71b64fc62e946c8f08b6ce

fixed iso version and md5

build.sh
@@ -48,8 +48,8 @@ UDEB_NAME="rpcs-pre rpcs-post kvmcheck pingcheck eula"
 CHEF_IMAGE_NAME="chef-server.qcow2"
 CHEF_IMAGE_HOST=${CHEF_IMAGE_HOST:-c390813.r13.cf1.rackcdn.com}
 CHEF_IMAGE_URL="http://${CHEF_IMAGE_HOST}/${CHEF_IMAGE_NAME}"
-ISO_URL="http://releases.ubuntu.com/precise/ubuntu-12.04-server-amd64.iso"
-ISO_MD5="f2e921788d35bbdf0336d05d228136eb"
+ISO_URL="http://releases.ubuntu.com/precise/ubuntu-12.04.1-server-amd64.iso"
+ISO_MD5="a8c667e871f48f3a662f3fbf1c3ddb17"
 ISO_CUSTOM="rpcs-pridery.iso"
 CIRROS_IMAGE_NAME="cirros-0.3.0-x86_64-uec.tar.gz"
 CIRROS_URL="https://launchpadlibrarian.net/83305869/${CIRROS_IMAGE_NAME}"

commit 13e3237f097a594b3ce83c60c4a50a140593625f

Fix md5 checking on the downloaded iso
4de19f66533a7a4f1d71b64fc62e946c8f08b6ce broke this.  fixing.

build.sh
@@ -118,7 +118,7 @@ fi
 
 # download the installation disk if we haven't already or it is corrupted somehow
 if [ -e "$ISO_FILENAME" ]; then
-  if [[ "$PLATFORM" = "osx" ]] && [[ $(md5 -q "$ISO_FILENAME") = "$ISO_MD5" ]] || [ $ISO_MD5 != `md5sum $ISO_FILENAME | cut -d ' ' -f1` ]; then
+  if [[ "$PLATFORM" = "osx" && $(md5 -q "$ISO_FILENAME") != "$ISO_MD5" ]] || [[ "$PLATFORM" != "osx" && $ISO_MD5 != `md5sum $ISO_FILENAME | cut -d ' ' -f1` ]]; then
     echo "Removing bad iso"
     rm "$ISO_FILENAME"
   fi